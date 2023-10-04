170782a2bed271c82cbf3b48e85d8d9ccbbeb96f
Anton_Andreev2 <anton_andreev2@epam.com>
2023-10-04 15:49:09 +0300

    Css_xpath

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -15,4 +15,26 @@ describe("Test suite", () =>{
         const emailError = await $("label#Email-info");
         expect(await emailError.getText()).toEqual("Enter valid email");
     });
+
+    it("Third TEST", async () => {
+        const element = await $("//div[contains(@class, 'patients')]");
+        await browser.execute("arguments[0].click();", element);
+        await $("button=Add New Patient").click();
+        await $("//input[@name='Name']").setValue("John Doe");
+        await $("button=Save").click();
+    
+        const emailError = await $("label[id='Email-info']");
+        expect(await emailError.getText()).toEqual("Enter valid email");
+    });
+
+    it("fourth TEST", async () => {
+        await browser.url("https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard");
+        await $("a=View All").click();
+        await $("//button[text()='Add New Doctor']").click();
+        await $("//input[@name='Name']").setValue("John Doe");
+        await $("//button[text()='Save']").click();
+
+        const emailError = await $("label#Email-info");
+        expect(await emailError.getText()).toEqual("Enter valid email");
+    });
 });
