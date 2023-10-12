0db7723d0d2d1852ed9402d9a7f634b92b8bf7ae
Anton_Andreev2 <anton_andreev2@epam.com>
2023-10-12 15:59:29 +0300

    test

--- test/specs/test.spec.js
+++ test/specs/test.spec.js
@@ -4,7 +4,7 @@ describe("Test suite", () =>{
         await browser.url("https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard");
       });
 
-    it("First test", async () => {
+      it("First test", async () => {
         const pageTitle = await browser.getTitle();
     console.log(pageTitle);
      });
@@ -41,13 +41,11 @@ describe("Test suite", () =>{
     });
     it("fifth TEST", async () => {
         await $("div.doctors").click();
-
         await expect(browser).toHaveUrlContaining('doctors')
     });
 
     it("Sixth TEST", async () => {
         await browser.url("https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/patients");
-
         await expect(browser).toHaveTitle('Appointment Planner - Syncfusion Angular Components Showcase App')
     });
 
@@ -57,88 +55,22 @@ describe("Test suite", () =>{
   
     it('should fetch menu links and visit each page', async () => {
         const menuItems = await browser.$$('ejs-sidebar#plan nerSiderBar div.sidebar-item');
-        
         await menuItems.forEach(async (link) => {
             await link.click()
         })
-        await $("a=View All").click();
-        
-        await expect(browser).toHaveUrlContaining('doctors')
-
     })
 
-    it ('should check planner is about is Displayed', async () => {
-        await $("div.calendar").waitForAndClick(); //custom click
-
-        let elem = await $("div.planner-calendar");
+    it ('should check is about is Displayed', async () => {
+        let elem = await $("div.about");
         let isDisplayed = await elem.isDisplayed();
-
         await expect(isDisplayed).toEqual(true)
     });
 
-    it('should detect when wrapper is visible', async () => {
-        await browser.url("https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/patients");
-        const elem = await $("div.patients-detail-wrapper");
-        
-        await elem.waitForDisplayed({ timeout: 200})
+    it('should detect when element is visible', async () => {
+        const elem = await $("div.preference");
+        await elem.waitForDisplayed({ timeout: 200 })
+
     })
     
-    
-    it('1 should inject javascript on the add new doctor page', async () => {
-        const result = await browser.execute((a, b, c, d) => {
-            return a + b + c + d
-        }, 1, 2, 3, 4)
-        await $("div.doctors").click();
-        await $("//button[text()='Add New Doctor']").click();
-        await $("input[name='Name']").setValue(result);
-        await $("//button[text()='Save']").click();
-        const emailError = await $("label#Email-info");
-        expect(await emailError.getText()).toEqual("Enter valid email");
-    });
- 
-    it('2 should wait until chart is displayed', async () => {
-        const selector = '#chartcontainer_ChartBorder';
-        await browser.waitUntil(async () => {
-            return (await browser.$(selector).isExisting()) && (await browser.$(selector).isDisplayed());
-        }, {
-            timeout: 100,
-            timeoutMsg: 'Expected element to be visible after 0,1 seconds'
-        });
-    });
-
-    it('3 Should moveTo about button and click', async () => {
-        const button = await browser.$("div.about");
-        await button.moveTo(); //browser action
-        await button.click();
-        await expect(browser).toHaveUrlContaining('about')
-    });
-
-    it('4 should add cookies and and check', async () => {
-        const primaryCookies = [
-            {name: 'This one to check', value: 'check'},
-            {name: 'This one to be sure', value: 'sure'}
-        ];
-        await browser.deleteCookies();
-        await browser.setCookies(primaryCookies);
-        const cookies = await browser.getCookies();
-    
-        primaryCookies.forEach(cookie => {
-            const foundCookie = cookies.find(c => c.name === cookie.name && c.value === cookie.value);
-            expect(foundCookie).toBeDefined();
-        });
-    });
-
-    it('should add elements asynchronously', async () => {
-        await browser.executeAsync((done) => {
-            setTimeout(() => {
-                const div = document.createElement('div');
-                div.id = 'newElement';
-                document.body.appendChild(div);
-                done();
-            }, 1000);
-        });
-        const element = await browser.$('#newElement');
-        expect(element).toBeDefined();
-    });
     
 });
