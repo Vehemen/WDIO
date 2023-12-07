3a0ace6e471952a935ccb1e5d3d1fd53b3f2fc55
Anton_Andreev2 <anton_andreev2@epam.com>
2023-12-07 23:34:16 +0300

    Firefox

--- src/config/wdio.conf.js
+++ src/config/wdio.conf.js
@@ -56,7 +56,6 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'}, {
         browserName: 'firefox'}
     ],
 
